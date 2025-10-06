9ff851eead45b22e247be467fd9bc1173d483482
john125892 <johnsteven747474@gmail.com>
2025-10-06 08:42:24 -0500

    edit3 8:42am

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-import { obtenerFichas } from '';
+import { obtenerFichas } from './fichas-adso.js';
 
-obtenerFichas().then(fichas => {
-  console.log(fichas);
-});
+const selectFicha = document.getElementById('selectFicha');
+
+async function cargarFichas() {
+  const datos = await obtenerFichas();
+
+  datos.fichas.forEach(ficha => {
+    const option = document.createElement('option');
+    option.value = ficha.url;
+    option.textContent = ficha.codigo;
+    selectFicha.appendChild(option);
+  });
+}
+
+cargarFichas();
